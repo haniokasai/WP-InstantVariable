661bd610de9987df381691b1c570816d245b1430
haniokasai <htek@haniokasai.com>
2019-03-13 13:10:58 +0900

    wpinit

---
+++ InstantVariable.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  Plugin Name: InstantVariable
+  Plugin URI: https://github.com/haniokasai/WP-InstantVariable
+  Description: 自由な変数をパッと追加する
+  Version: 0.0.1
+  Author: Tomoaki TANAKA
+  Author URI: https://haniokasai.com
+  License: MIT License
+ */
